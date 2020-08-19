fa62ff5dac04b680948e83483b5e55780fc19db0
Tatiana Inama <tatii.oxo@gmail.com>
2020-08-19 12:03:35 +0200

    Add Konva. Create board generators

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Stage, Layer, Text } from 'react-konva';
+
 import './App.css';
 
+const mkLifeState = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+const mkGameBoard = (width, height) => {
+  const board = Array.from({ length: height }, (_, i) => Array.from({ length: width }, (_, j) => mkLifeState(2)));
+  return board;
+}
+
+const printBoard = (board) => {
+  console.log(`${board.map(row => row.map(i => i ? '#' : ' ').join(' ')).join('\n')}`)
+}
+
 function App() {
+  const board = mkGameBoard(10, 10);
+  printBoard(board);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stage width={300} height={300}>
+        <Layer>
+          <Text text="Game of life"></Text>
+        </Layer>
+
+      </Stage>
     </div>
   );
 }
